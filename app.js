518f7f591b0ee063704f487afd5168f23187c680
Suraj Chodankar <suraj.chodankar2209@gmail.com>
2019-10-26 15:15:41 +0530

    =dk

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const request = require('request')
-url = 'https://api.darksky.net/forecast/4000d4d40e964c055d8ad654e546d189/37.8267,-122.4233'
-request({url: url }, (error,response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+url = 'https://api.darksky.net/forecast/4000d4d40e964c055d8ad654e546d189/37.8267,-122.4233?units=si'
+request({url: url, json: true }, (error,response)=>{
+    console.log(response.body.daily.data[0].summary + " The temperature today is " + response.body.currently.temperature + " degree centigrade and there is " + response.body.currently.precipProbability + "% probability of rain today!")
 })
