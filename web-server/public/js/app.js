2a93850e9444cdbcab2f284141467d74fe220340
Suracho <suracho@outlook.com>
2020-03-16 18:44:11 +0530

    added some css on the page and worked on the interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,16 +2,22 @@ console.log('Client side javascript file is loaded!')
 
 const form = document.querySelector('form')
 const input = document.querySelector('input')
+const message1 = document.querySelector('#message1')
+const message2 = document.querySelector('#message2')
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = input.value
+    message1.textContent = 'loading'
+    message2.textContent =''
     fetch('http://localhost:3000/weather?address='+encodeURIComponent(location)).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
-         console.log(data.error)
+         message1.textContent = data.error
         }
         else{
-            console.log(data)
+            message1.textContent = data.forecasts
+            message2.textContent = data.place_name
         }
         
     })
