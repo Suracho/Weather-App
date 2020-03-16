7c5c832550db8b84de171c6509ca10724dfde6e8
Suracho <suracho@outlook.com>
2020-03-16 14:55:35 +0530

    added some client side js, form and search

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,19 @@
 console.log('Client side javascript file is loaded!')
+
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const location = input.value
+    fetch('http://localhost:3000/weather?address='+encodeURIComponent(location)).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+         console.log(data.error)
+        }
+        else{
+            console.log(data)
+        }
+        
+    })
+})
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,15 +10,12 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
-
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
-
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
